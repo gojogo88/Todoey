4827f3f94cfd450a3ab97f91518eef05997d429b
Jonathan Go <jogo88@gmail.com>
2018-11-24 16:29:09 +0900

    changed to MVC, fixed tableview checkmark bug but broke user defaults.

---
+++ Todoey/Models/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Jonathan Go on 2018/11/24.
+//  Copyright © 2018 Appdelight. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+  var title = ""
+  var done = false
+}
