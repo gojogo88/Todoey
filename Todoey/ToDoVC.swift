916063c4f39b7889344ecf74114c8b52f7012c0b
Jonathan Go <jogo88@gmail.com>
2018-11-24 14:25:02 +0900

    created itemArray and setup tableview for testing.

--- Todoey/ToDoVC.swift
+++ Todoey/ToDoVC.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class ToDoVC: UITableViewController {
 
+  let itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+  
   override func viewDidLoad() {
     super.viewDidLoad()
 
   }
 
-
+  //Mark - Tableview Datasource Methods
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return itemArray.count
+  }
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+    cell.textLabel?.text = itemArray[indexPath.row]
+    return cell
+  }
+  
 }
 
